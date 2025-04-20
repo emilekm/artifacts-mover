4a0979e6a49d80e74a024f35d43a8edf1f6d938a
cassius23 <emilekm@gmail.com>
2025-04-20 20:38:59 +0200

    Split uploaders into own files

---
+++ internal/uploader_scp.go
@@ -0,0 +1,76 @@
+package internal
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/emilekm/artifacts-mover/internal/config"
+	"github.com/povsister/scp"
+	"golang.org/x/crypto/ssh"
+)
+
+type scpUploader struct {
+	artifactsConfig config.ArtifactsConfig
+
+	basePath string
+	address  string
+	scpConf  *ssh.ClientConfig
+}
+
+func newSCPUploader(
+	conf config.SCPConfig,
+	artifactsConfig config.ArtifactsConfig,
+) (*scpUploader, error) {
+	privKey, err := os.ReadFile(conf.PrivateKeyFile)
+	if err != nil {
+		return nil, err
+	}
+
+	scpConf, err := scp.NewSSHConfigFromPrivateKey(conf.Username, privKey)
+	if err != nil {
+		return nil, err
+	}
+
+	u := &scpUploader{
+		artifactsConfig: artifactsConfig,
+		basePath:        conf.BasePath,
+		address:         conf.Address,
+		scpConf:         scpConf,
+	}
+
+	_, err = u.client()
+	if err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
+
+func (u *scpUploader) Upload(round Round) error {
+	client, err := u.client()
+	if err != nil {
+		return err
+	}
+
+	for typ, path := range round {
+		err := client.CopyFileToRemote(
+			path,
+			u.fullUploadPath(typ, path),
+			&scp.FileTransferOption{},
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (u *scpUploader) fullUploadPath(typ config.ArtifactType, path string) string {
+	filename := filepath.Base(path)
+	return filepath.Join(u.basePath, u.artifactsConfig[typ].UploadPath, filename)
+}
+
+func (u *scpUploader) client() (*scp.Client, error) {
+	return scp.NewClient(u.address, u.scpConf, &scp.ClientOption{})
+}

b0b08bd4f074603c1e86a9f7bbfe0ed448749f15
cassius23 <emilekm@gmail.com>
2026-07-31 01:26:17 +0200

    Simplify handling logic

--- internal/types.go
+++ internal/types.go
@@ -6,3 +6,26 @@ type Artifact struct {
 	Path string
 	Type config.ArtifactType
 }
+
+func (h *Handler) handleFile2(artifact Artifact) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	if _, ok := h.currentRound[artifact.Type]; ok {
+		h.endCurrentRoundLocked()
+	}
+	if artifact.Type == config.ArtifactTypeBF2Demo && len(h.currentRound) > 0 {
+		h.endCurrentRoundLocked()
+	}
+
+	h.currentRound[artifact.Type] = artifact
+
+	if len(h.currentRound) == h.typesCount {
+		h.endCurrentRoundLocked()
+		return
+	}
+
+	if len(h.currentRound) == 1 && h.roundTimeout > 0 {
+		h.startRoundTimer()
+	}
+}
